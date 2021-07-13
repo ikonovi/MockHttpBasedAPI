191a6bbcaa2f4cc712e2730597f5cc26fa02ae1d
Igor Konovalov <ikonovi@gmail.com>
2021-07-13 18:06:01 +0300

    added ability to generate random String of defined length

--- src/main/java/ik/util/random/RandomGenerator.java
+++ src/main/java/ik/util/random/RandomGenerator.java
@@ -18,14 +18,25 @@ public class RandomGenerator {
         int lettersCapitalMaxAsciCode = 90; // character 'Z'
         int lettersLowercaseMinAsciCode = 97; // character 'a'
         int lettersLowercaseMaxAsciCode = 122; // character 'z'
-
         Random random = ThreadLocalRandom.current();
-            return random
-                    .ints(numbersMinAsciCode, lettersLowercaseMaxAsciCode + 1)
-                    .filter(i -> (i <= numbersMaxAsciCode || i >= lettersCapitalMinAsciCode)
-                            && (i <= lettersCapitalMaxAsciCode || i >= lettersLowercaseMinAsciCode))
-                    .limit(length)
-                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                    .toString();
+        return random
+                .ints(numbersMinAsciCode, lettersLowercaseMaxAsciCode + 1)
+                .filter(i -> (i <= numbersMaxAsciCode || i >= lettersCapitalMinAsciCode)
+                        && (i <= lettersCapitalMaxAsciCode || i >= lettersLowercaseMinAsciCode))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    /**
+     * Returns random alphanumeric string in range between min and max
+     * that are positive numbers.
+     */
+    public String randomAlphanumericFromRange(int lengthMin,  int lengthMax) {
+        int lengthMaxInclusive = lengthMax + 1;
+        Random random = ThreadLocalRandom.current();
+        int intInDelta = random.nextInt(lengthMaxInclusive - lengthMin);
+        int intInMinMax = lengthMin + intInDelta;
+        return randomAlphanumeric(intInMinMax);
     }
 }
